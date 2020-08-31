ad6e7cbce3229a15a005c320ad7214f31dc1116e
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-08-31 12:30:24 -0500

    Updates

---
+++ precache-manifest.5fb2a16a26296ea68afd1840ace3197c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de07f0b34ff1e0cc825a5cf5b63a064e",
+    "url": "/app-vercel/index.html"
+  },
+  {
+    "revision": "3a81a90803d46b1f71e3",
+    "url": "/app-vercel/static/css/2.44390fd7.chunk.css"
+  },
+  {
+    "revision": "8f925866fd088969c2be",
+    "url": "/app-vercel/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3a81a90803d46b1f71e3",
+    "url": "/app-vercel/static/js/2.f43690d9.chunk.js"
+  },
+  {
+    "revision": "ded0dad0a9aac4f2c1b5bbb99c4e4de4",
+    "url": "/app-vercel/static/js/2.f43690d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f925866fd088969c2be",
+    "url": "/app-vercel/static/js/main.77ccb0f5.chunk.js"
+  },
+  {
+    "revision": "6d17c0f011cd090c82e1",
+    "url": "/app-vercel/static/js/runtime-main.586bd113.js"
+  },
+  {
+    "revision": "b201ac5c25e33051018b86bf5b1f2540",
+    "url": "/app-vercel/static/media/bascula-dibujo.b201ac5c.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/app-vercel/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/app-vercel/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/app-vercel/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/app-vercel/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/app-vercel/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
